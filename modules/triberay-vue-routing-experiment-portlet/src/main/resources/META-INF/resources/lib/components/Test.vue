ad3a2795f7cce322d14da2a4d8bd51f0dc749298
Wouter Vernaillen <wouter@vernaillen.com>
2019-04-19 23:08:51 +0200

    added state with VueX and test with counter

--- modules/triberay-vue-routing-experiment-portlet/src/main/resources/META-INF/resources/lib/components/Test.vue
+++ modules/triberay-vue-routing-experiment-portlet/src/main/resources/META-INF/resources/lib/components/Test.vue
@@ -1,10 +1,33 @@
 <template>
     <div>
         <h1>Test Page</h1>
+        <p>created counter: {{ createdCounter }}</p>
+        <p>click counter: {{ clickCounter }}</p>
+        <p><button @click="increaseCounter()">Increase counter</button></p>
     </div>
 </template>
 
 <script>
+    import { store } from '../store';
+
+    export default {
+        data () {
+            return {
+                createdCounter: store.state.createdCounter,
+                clickCounter: store.state.clickCounter
+            }
+        },
+        created: function () {
+            store.state.createdCounter = store.state.createdCounter + 1;
+            console.log("created method, counter is now " + store.state.createdCounter);
+        },
+        methods: {
+            increaseCounter: function() {
+                store.state.clickCounter = store.state.clickCounter + 1
+                this.clickCounter = store.state.clickCounter;
+            }
+        }
+    }
 </script>
 
 <style scoped>
